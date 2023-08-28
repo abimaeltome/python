13d0fdd08a88b254773ecfc9a657deeb33652655
Abimael Pereira Tomé <142822485+abimaeltome@users.noreply.github.com>
2023-08-28 11:05:42 -0300

    Revert "atualização do trabalaho"
    This reverts commit 5ddb2ac22386f89d5db459eb7b18bf1d7ad5fac5.

--- instrução_pass.py
+++
@@ -1,7 +0,0 @@
-#A instrução pass atua sobre a estrutura if, permitindo que ela seja escrita sem outras instruções a serem executadas caso a condição seja verdadeira.
-for num in range(1, 11):
-    if num % 2 == 0:
-        pass
-    else:
-        print(num)
-print('Laço encerrado. ')
