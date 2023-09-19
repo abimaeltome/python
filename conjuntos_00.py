8c69246a49293f01f005d8f87079ea51b7bff113
Abimael Pereira Tomé <142822485+abimaeltome@users.noreply.github.com>
2023-09-19 17:06:18 -0300

    oksdsd

---
+++ conjuntos_00.py
@@ -0,0 +1,18 @@
+""" Conjuntos (Set)
+
+Em Python, a classe responsável por implementar os Conjuntos se chama Set.
+Os Conjuntos podem ser utilizados para remover itens duplicados de outras sequências ou testes de pertinência e computar operações matemáticas, como união e interseção, entre outras. """
+
+"""Definindo um conjunto
+Para inicializarmos um objeto do tipo Set, podemos proceder de duas formas: """
+
+""" Utilizar chaves com elementos separádos por vírgula"""
+
+# a.conjunto = {1,2,3}
+
+""" Utilizar o construtor set(), que aceita como parâmetros outras coleções """
+
+# a.conjunto = set()
+# a.conjunto = set(1,2,3)
+# a.conjunto = set([1,2,3])
+
